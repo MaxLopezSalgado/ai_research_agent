482b1b05e4b87e70ec1e2391942f09622d834b00
MaxLopezSalgado <maxlopezsalgado@gmail.com>
2023-07-31 18:18:24 +0200

    searching app running

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ from langchain.chat_models import ChatOpenAI
 from langchain.prompts import MessagesPlaceholder
 from langchain.memory import ConversationSummaryBufferMemory
 from langchain.text_splitter import RecursiveCharacterTextSplitter
-from langchain.chains.sumarize import load_sumarize_chain
+from langchain.chains.summarize import load_summarize_chain
 from langchain.tools import BaseTool
 from pydantic import BaseModel, Field
 from typing import Type
@@ -26,7 +26,25 @@ serper_api_key = os.getenv('SERP_API_KEY')
 
 
 # 1. Tool for searching
+def search(query):
+    url = "https://google.serper.dev/search"
 
+    payload = json.dumps({
+    "q": query
+    })
+
+    headers = {
+    "X-API-KEY": serper_api_key,
+    "Content-Type": 'application/json'
+    }
+
+    response = requests.request("POST", url, headers=headers, data=payload)
+
+    print(response.text)
+
+    return response.text
+
+search("what is meta's thread product?")
 
 # 2. Tool for scraping
 

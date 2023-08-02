01107bbbacf92a72b6ed4416b6c1ea42fd8d3264
MaxLopezSalgado <maxlopezsalgado@gmail.com>
2023-08-02 17:58:59 +0200

    scrapping tool running

--- app.py
+++ app.py
@@ -85,6 +85,47 @@ def scrape_website(objective: str, url: str):
     else:
         print(f"Http request failed with status code: {response.status_code}")
 
+def summary(objective, content):
+    llm = ChatOpenAI(temperature=0, model="gpt-3.5-turbo-16k-0613")
+
+    text_splitter = RecursiveCharacterTextSplitter(
+        separators =["\n\n", "\n"], chunk_size=10000, chunk_overlap=500)
     
+    docs = text_splitter.create_documents([content])
+
+    map_prompt = """
+    Write a summary of the following text for {objective}:
+    "{text}"
+    Summary: 
+    """
+
+    map_prompt_template = PromptTemplate(
+        template = map_prompt, imput_variables=["text", "objective"])
+    
+    summary_chain = load_summarize_chain(
+        llm = llm,
+        chain_type = map_prompt_template,
+        combine_prompt = map_prompt_template, 
+        verbose = True
+    )
+
+    output = summary_chain.run(imput_documents= docs, objective= objective)
+
+    return output
+
+class ScrapeWebsiteInput(BaseModel):
+    objective: str = Field(Description= "The objective & taks that the user give to the agent")
+    url: str = Field(description= "The url of the webseite to be scraped")
+
+class ScrapeWebsiteTool(BaseTool):
+    name = "scrape_website"
+    description = "useful when you need to get data from a website url, passing both url and objective to the function; DO NOT make up any url, the url should only be from the search results"
+    args_schema: Type[BaseModel] = ScrapeWebsiteInput
+
+    def _run(self, objective: str, url: str):
+        return scrape_website(objective, url)
+
+    def _arun(self, url: str):
+        raise NotImplementedError("error here")
 
 # 3. Create a langchain agent with tools above
